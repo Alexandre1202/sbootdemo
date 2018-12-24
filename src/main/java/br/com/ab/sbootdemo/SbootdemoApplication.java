323828a0372e13df869fa9c6e614b9a1b76ee0db
alexandre1202 <alexandre1202@gmail.com>
2018-12-24 11:38:14 -0200

    First commit of CRUD using Spring Boot

---
+++ src/main/java/br/com/ab/sbootdemo/SbootdemoApplication.java
@@ -0,0 +1,14 @@
+package br.com.ab.sbootdemo; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootdemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/ab/sbootdemo/sbootdemo/SbootdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.ab.sbootdemo.sbootdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbootdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
