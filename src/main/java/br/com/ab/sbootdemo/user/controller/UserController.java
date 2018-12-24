69e19cf26cf66713bc7df77f6672ffcc1b187ff4
alexandre1202 <alexandre1202@gmail.com>
2018-12-24 15:23:29 -0200

    Adicao da obtencao de lista de usuarios

--- src/main/java/br/com/ab/sbootdemo/user/controller/UserController.java
+++ src/main/java/br/com/ab/sbootdemo/user/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.ab.sbootdemo.user.model.UserModel;
 import br.com.ab.sbootdemo.user.repository.UserRepository;
@@ -21,13 +22,15 @@ public class UserController {
 
 	@RequestMapping(value="/userView", method=RequestMethod.POST)
 	public String userView(UserModel user) {
-		System.out.println("Vai salvar");
 		userRepository.save(user);
 		return "redirect:/userView";		
 	}
 	
-//	@RequestMapping("/")
-//	public String index() {
-//		return "index";
-//	}
+	@RequestMapping("/users")
+	public ModelAndView userList() {
+		ModelAndView mav = new ModelAndView("index");
+		Iterable<UserModel> users = userRepository.findAll();
+		mav.addObject("users", users);
+		return mav;
+	}
 }
