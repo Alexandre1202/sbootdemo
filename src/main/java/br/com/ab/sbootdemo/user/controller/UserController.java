ef4076c07859d03bf1189d92afd8849c4ee6acd0
alexandre1202 <alexandre1202@gmail.com>
2018-12-25 11:31:17 -0200

    Adicao da view de lista de usuarios

--- src/main/java/br/com/ab/sbootdemo/user/controller/UserController.java
+++ src/main/java/br/com/ab/sbootdemo/user/controller/UserController.java
@@ -2,6 +2,7 @@ package br.com.ab.sbootdemo.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,6 +32,14 @@ public class UserController {
 		ModelAndView mav = new ModelAndView("index");
 		Iterable<UserModel> users = userRepository.findAll();
 		mav.addObject("users", users);
+		return mav;
+	}
+	
+	@RequestMapping("/{id}")
+	public ModelAndView userDetail(@PathVariable("id") long id) {
+		UserModel userModel = userRepository.findById(id);
+		ModelAndView mav = new ModelAndView("userDetail");
+		mav.addObject("user", userModel);
 		return mav;
 	}
 }
--- src/main/java/br/com/ab/sbootdemo/user/repository/UserRepository.java
+++ src/main/java/br/com/ab/sbootdemo/user/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.ab.sbootdemo.user.model.UserModel;
 
-public interface UserRepository extends CrudRepository<UserModel, String>{
-
+public interface UserRepository extends CrudRepository<UserModel, String> {
+	UserModel findById(long id);
 }
