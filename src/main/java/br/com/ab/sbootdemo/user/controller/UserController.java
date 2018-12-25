c895d7d6fecdc5a606bfbe0a40cb27dcfa07b003
alexandre1202 <alexandre1202@gmail.com>
2018-12-25 19:32:59 -0200

    Adicao de validacao em Usuarios e enderecos

--- src/main/java/br/com/ab/sbootdemo/user/controller/UserController.java
+++ src/main/java/br/com/ab/sbootdemo/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package br.com.ab.sbootdemo.user.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.ab.sbootdemo.user.model.AddressModel;
 import br.com.ab.sbootdemo.user.model.UserModel;
@@ -27,8 +31,13 @@ public class UserController {
 	}
 
 	@RequestMapping(value="/userView", method=RequestMethod.POST)
-	public String userView(UserModel user) {
+	public String userView(@Valid UserModel user, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Invalid data-entry!");
+			return "redirect:/userView";
+		}
 		userRepository.save(user);
+		attributes.addFlashAttribute("mensagem", "User has added successfully!");
 		return "redirect:/userView";		
 	}
 	
@@ -51,10 +60,15 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.POST)
-	public String userDetail(@PathVariable("id") long id, AddressModel addressModel) {
+	public String userDetail(@PathVariable("id") long id, @Valid AddressModel addressModel, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Invalid data-entry!");
+			return "redirect:/{id}";
+		}
 		UserModel userModel = userRepository.findById(id);
 		addressModel.setUserModel(userModel);
 		addressRepository.save(addressModel);
+		attributes.addFlashAttribute("mensagem", "Address has added succcessfully!");
 		return "redirect:/{id}";
 	}
 
